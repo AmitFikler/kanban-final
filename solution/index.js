700837e190363631f1d9d41ff6480cfd2039522c
AmitFikler <amitfikler@gmail.com>
2021-09-22 15:47:09 +0300

    build basic HTML

---
+++ solution/index.js
@@ -0,0 +1,9 @@
+// const submit = document.getElementById("submit-add-to-do")
+// submit.addEventListener("click",chack)
+// function chack() {
+//     const value = document.getElementById("add-to-do-task").value
+//     const list = document.createElement("li")
+//     list.textContent = value
+//     document.getElementById("to-do-tasks").appendChild(list)
+
+// }
