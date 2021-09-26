d1924c21ec299e168da3e6f1ad2a87c0dc993312
AmitFikler <amitfikler@gmail.com>
2021-09-26 11:19:05 +0300

    ✔️ Attach a function to transfer tasks

--- solution/index.js
+++ solution/index.js
@@ -11,13 +11,16 @@ const ulInProgress  = document.getElementsByClassName("in-progress-tasks")[0]
 const ulDone  = document.getElementsByClassName("done-tasks")[0]
 
 const buttonEl = document.getElementsByTagName("button")
+const title = document.getElementById("page-title")
+
+const liElements = document.getElementsByTagName("li")
 
 
 const searchBar = document.getElementById("search")
 searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value
+    const searchString = e.target.value.toLowerCase()
     for (let i of liElements) {
-        if (!i.textContent.includes(searchString)) {
+        if (!i.textContent.toLowerCase().includes(searchString)) {
             i.style.display = "none"
         }
         else {
@@ -31,23 +34,55 @@ const tasks = {
     "in-progress": [],
     "done": []
 }
+document.body.style.backgroundImage = localStorage.getItem("background")
 
 const getLocalStorageTasks = JSON.parse(localStorage.getItem("tasks"))
 
-const liElements = document.getElementsByTagName("li")
-
-function handleClickEvents() {
-    for (li of liElements) {
-    li.addEventListener("dblclick", listenForDoubleClick)
+let element = undefined
+let focos = undefined
+function handleMouseEvents(event) {
+    if (event.target.classList.contains("task")){
+        const ele = event.target
+        const eleText = event.target.textContent
+        element = ele
+        focos = eleText
+        document.addEventListener("keydown", (e) => {
+            if(e.altKey && e.key === "1") {
+                if (focos !== undefined) {
+                    tasks["todo"].push(focos)
+                    tasks[findparents(element)].splice(findparents(element).indexOf(focos),1)
+                    ulToDo.insertBefore(element,ulToDo.firstChild)
+                    localStorage.setItem("tasks",JSON.stringify(tasks))
+                    focos = undefined
+                }
+            }
+            if (e.altKey && e.key === "2") {
+                if (focos !== undefined) {
+                    tasks["in-progress"].push(focos)
+                    tasks[findparents(element)].splice(findparents(element).indexOf(focos),1)
+                    ulInProgress.insertBefore(element,ulInProgress.firstChild)
+                    localStorage.setItem("tasks",JSON.stringify(tasks))
+                    focos = undefined
+                }
+            }
+            if (e.altKey && e.key === "3") {
+                if (focos !== undefined) {
+                    tasks["done"].push(focos)
+                    tasks[findparents(element)].splice(findparents(element).indexOf(focos),1)
+                    ulDone.insertBefore(element,ulDone.firstChild)
+                    localStorage.setItem("tasks",JSON.stringify(tasks))
+                    focos = undefined
+                }
+            }
+        })
     }
 }
 
-function handleMouseEvents() {
-    for (li of liElements) {
-        li.addEventListener("mouseover", altPress)
-    }
-}
 
+const body = document.getElementById("body")
+document.addEventListener('mouseover', handleMouseEvents)
+document.querySelectorAll(".add-task").forEach((btn) => btn.addEventListener("click", chack))
+document.addEventListener("dblclick", listenForDoubleClick)
 
 
 for (let key in getLocalStorageTasks) {
@@ -72,12 +107,6 @@ function addStorageToDom(task,ul,key) {
     ul.insertBefore(list,ul.firstChild)
 }
 
-handleClickEvents()
-handleMouseEvents()
-
-
-
-document.body.addEventListener("click",chack)
 function chack(e) {
     if (e.target.id === "submit-add-to-do"){
         if (!inputToDo.value) {
@@ -103,24 +132,17 @@ function chack(e) {
             localStorage.setItem("tasks",JSON.stringify(tasks))
         }
     }
-    handleMouseEvents()
-    handleClickEvents()
 }
 
-function altPress(event) {
-    console.log(event)
-
-    if (event.altKey===true) {
-       console.log(event.target)
-    }
-}
 
 function listenForDoubleClick(element) {
-    element.target.contentEditable = true;
-    element.target.onblur = function(){
-        updateUl(element.target)
-        localStorage.setItem("tasks", JSON.stringify(tasks))
-        element.target.contentEditable = false;
+    if (element.target.classList.contains("task")){
+        element.target.contentEditable = true;
+        element.target.onblur = function(){
+            updateUl(element.target)
+            localStorage.setItem("tasks", JSON.stringify(tasks))
+            element.target.contentEditable = false;
+        }
     }
 }
 
@@ -152,5 +174,63 @@ function updateUl(target) {
     }
 }
 
+
+function findparents(ele) {
+    const parent =  ele.parentElement.classList[0]
+    if (parent === "done-task") {
+        return "done"
+    }
+    else if (parent === "in-progress-tasks"){
+        return "in-progress"
+    }
+    else if (parent === "to-do-tasks") {
+        return "todo"
+    }
+}
+
+document.getElementById("background").addEventListener("click", changeBackground)
+function changeBackground() {
+    const url = document.getElementById("background-url")
+    document.body.style.backgroundImage = `url(${url.value})`
+    localStorage.setItem("background", `url(${url.value})`)
+}
+document.getElementById("background-default").addEventListener("click",defaultBackground)
+function defaultBackground() {
+    document.body.style.backgroundImage = `url("markus-winkler-3Rn2EjoAC1g-unsplash.jpg")`
+    localStorage.setItem("background", `url("markus-winkler-3Rn2EjoAC1g-unsplash.jpg")`)
+}
+
+document.getElementById("clear-tasks").addEventListener("click", (e)=> {
+    for (let type in tasks) {
+        tasks[type] = []
+    }
+    document.querySelectorAll("li").forEach((li) => li.remove())
+    localStorage.clear()
+})
+
+
+// async function getData() {
+//     const response = await fetch("https://json-bins.herokuapp.com/bin/614aece24021ac0e6c080c6f",{
+//         method: "GET"
+//     });
+//     if (!response.ok) {
+//         alert("error")
+//     }
+//     const data = await response.json()
+//     const tasksData = data.tasks   
+// }
+
+// async function savaData() {
+//     const response = await fetch("https://json-bins.herokuapp.com/bin/614aece24021ac0e6c080c6f", {
+//         method: "PUT",
+
+//     })
+// }
+
+
+// const b = document.createElement("button")
+// b.textContent = "press"
+// document.body.append(b)
+// b.addEventListener("click", getData)
 
 
